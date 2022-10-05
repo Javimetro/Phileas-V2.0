8e31426bd126a764c0a15460f3ce04952fa2534e
javimetro <javierj@metropolia.fi>
2022-10-05 13:56:17 +0300

    Aloitus

--- Javi/start.py
+++ Javi/start.py
@@ -1,5 +1,5 @@
 import mysql.connector
-import math
+from geopy.distance import geodesic
 
 yhteys = mysql.connector.connect(
          host='127.0.0.1',
@@ -29,7 +29,7 @@ def haelatitude():
 lat1 = haelatitude()
 lon1 = haelongitude()
 
-distance = int(input(f'Kuinka monta kilometria haluat lentaa? ')) # kilometreina
+distance = int(input(f'Kuinka monta kilometria haluat lentää? ')) # kilometreina
 
 northlimit = lat1[0] + distance*0.01
 southlimit = lat1[0] - distance*0.01
@@ -43,10 +43,43 @@ def valikoima():
     sql = f'''SELECT ident, name, latitude_deg, longitude_deg
     FROM Airport WHERE latitude_deg BETWEEN {southlimit} AND {northlimit}
     AND longitude_deg BETWEEN {eastlimit} AND {westlimit}'''
-    print(sql)
     kursori = yhteys.cursor()
     kursori.execute(sql)
     tulos = kursori.fetchall()
     return tulos
 
 print(valikoima())
+
+
+mihin = input(f'Mihin haluaisit lentää seuraavaksi? (Kirjoita lentokentän ICAO-koodin) ')
+
+def etaisyysicaolla(icao):
+    tuple = (icao,)
+    sql = '''SELECT latitude_deg, longitude_deg 
+    FROM airport 
+    WHERE ident = %s'''
+    kursori = yhteys.cursor()
+    kursori.execute(sql, tuple)
+    tulos = kursori.fetchone()
+    print(tulos)
+    return tulos
+
+icao1 = 'EGLC'
+icao2 = mihin
+
+etäisyys = print(f' Etäisyys lentokenttien välillä on: {round(geodesic(etaisyysicaolla(icao1), etaisyysicaolla(icao2)).km,3)} Km.')
+varmistus = input(f'Oletko varma haluatko matkustaa {mihin} ICAO-koodinen lentokentälle (K/E)?: ')
+
+while varmistus == 'K':
+    distance = int(input(f'Kuinka monta kilometria haluat lentää? '))
+    print(valikoima())
+    print(etäisyys)
+    print(varmistus)
+
+def updatelocation():
+    sql = '''UPDATE game SET location =''' icao2
+    print(sql)
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    if kursori.rowcount == 1:
+        print("Location päivitetty")
