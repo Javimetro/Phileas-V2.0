9e6237a72fdfd0e09e4d70b2828865e3c63e2a07
Lenni Marjomaa <lennima@metropolia.fi>
2022-10-11 11:15:35 +0300

    Lisätty game over funktio, koodi kirjoitettu useassa paikassa uudelleen.

--- Lenni/budjetti.py
+++ Lenni/budjetti.py
@@ -9,14 +9,15 @@ yhteys =mysql.connector.connect(
          )
 #nämä ovat placeholdereita kokeiluja varten
 budjetti_1=1000
-matkakustannus=700
+matkakustannus=100000
 alennus=500
+peli_id=2
 print(f'Sinut on haastettu matkaamaan maailman ympäri. Hyvää onnea!')
 
 #Testaan aluksi että budjetin päivitys tietokantaan onnistui
 
 def hae_budjetti():
-    sql=f'''SELECT co2_budget FROM game'''
+    sql=f'''SELECT co2_budget FROM peli WHERE id={peli_id} '''
     kursori = yhteys.cursor()
     kursori.execute(sql)
     tulos = kursori.fetchone()
@@ -24,15 +25,25 @@ def hae_budjetti():
 print(f"Budjettisi on alussa {hae_budjetti()}. Tämän lisäksi saat joka matkan jälkeen hieman lisärahaa.")
 
 #tämä osa ei toimi vielä
-def paivita_budjetti(alennus,matkahinta):
+def paivita_budjetti(alennus,matkakustannus):
 
-    sql=f'''UPDATE game SET co2_budget=co2_budget+{alennus}-{matkahinta}'''
+    sql=f'''UPDATE peli SET co2_budget=co2_budget+{alennus}-{matkakustannus} WHERE id={peli_id}'''
     print(sql)
     kursori=yhteys.cursor()
     kursori.execute(sql)
     tulos=kursori.fetchone()
     return tulos
 
-#laitetaan kaksi parametria
-paivita_budjetti(1,1000)
-print(hae_budjetti())
+
+
+
+def tarkista_budjetti():
+    sql=f'''SELECT screen_name FROM peli WHERE id={peli_id} AND co2_budget<=0'''
+    print(sql)
+    kursori=yhteys.cursor()
+    kursori.execute(sql)
+    tulos=kursori.fetchall()
+    return tulos
+
+if len(tarkista_budjetti())>0:
+    print("Peli ohi")
