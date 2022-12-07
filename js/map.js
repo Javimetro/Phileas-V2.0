247ef9cfe49641cb0affe407102f6a0816409e86
noorakansanaho <noora.kansanaho@metropolia.fi>
2022-12-07 12:16:38 +0200

    a

--- js/map.js
+++ js/map.js
@@ -6,3 +6,15 @@ L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
   subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
 }).addTo(map);
 map.setView([60, 24], 7);
+
+
+//pelaajan sijainti
+const marker = L.marker([51.505299, 0.055278]).addTo(map);
+marker.bindPopup(`AAA`);
+marker.openPopup();
+
+//ehdotetut lentokentät
+const flyhere = L.marker([53, -3]).addTo(map);
+flyhere.bindPopup(`AAA`);
+const suggested = L.divIcon({ className: 'suggested-icon' });
+flyhere.setIcon(suggested);
