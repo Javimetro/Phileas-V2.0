14652779fb8ce0bc813ff3f67bf6ff9d72cb4355
Lenni Marjomaa <lennima@metropolia.fi>
2022-12-08 17:06:03 +0200

    Säde-haku listaa konsoliin nut long ja lat tiedot nimillä kord1 ja kord2. Näitä käytetään myöhemmin antamaan palleroille oikeat sijainnit.

--- js/sade.js
+++ js/sade.js
@@ -1,25 +1,31 @@
-'use strict'
+'use strict';
 const url = 'http://127.0.0.1:5000/kilometria?id=1&km=';
 
-async function sade (evt) {
-  const maara = document.querySelector('#tasta').value;
+async function sade(evt) {
   evt.preventDefault();
+
+  const maara = document.querySelector('#tasta').value;
   console.log(maara);
   let sadeUrl = url + maara;
   console.log(sadeUrl);
 
-
-
-
   const response = await fetch(sadeUrl);
   const json = await response.json();
-  console.log(json)
-  const sade = json.kl_lkm;
-  console.log(sade);
-  //const elementti = document.querySelector('#tahan');
-  //elementti.innerText = sade;
+  console.log(json);
+  const sade = json.km_lkm;
+  const vaihtoehdo = json.vaihtoehdot;
+  console.log(vaihtoehdo);
+  for (let i = 0; i <= 9; i++) {
+    const kord1 = json.vaihtoehdot[i][2];
+    const kord2 = json.vaihtoehdot[i][3];
+    console.log(kord1)
+    console.log(kord2)
+
+  }
+
+  const elementti = document.querySelector('#tahan');
+  elementti.innerText = sade;
 }
 
-
 const nappi2 = document.querySelector('#paina');
-nappi2.addEventListener('click',sade);
+nappi2.addEventListener('click', sade);
