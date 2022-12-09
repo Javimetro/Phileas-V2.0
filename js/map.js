4ceb2af9c4b045d7a12850af1798bd374eaf2290
noorakansanaho <noora.kansanaho@metropolia.fi>
2022-12-09 13:00:23 +0200

    javascript yhdistetty

--- js/map.js
+++ js/map.js
@@ -8,6 +8,7 @@ L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
 }).addTo(map);
 map.setView([60, 24], 7);
 
+const apiUrl = 'http://127.0.0.1:5000/';
 
 //pelaajan nimi
 document.querySelector('#player-form').addEventListener('submit', function(evt){
@@ -15,7 +16,7 @@ document.querySelector('#player-form').addEventListener('submit', function(evt){
   const nimi = document.querySelector('#player-input').value;
   document.querySelector('#player-modal').classList.add('hide');
   togglePopup();
-  //gameSetup(apin url tähän);
+  //gameSetup(`${apiUrl}newgame?player=${playerName}&loc=${startLoc}`);
 })
 
 //Nappi (tarina&ohjeet)
@@ -67,3 +68,40 @@ document.querySelector('.goal').addEventListener('click', function (evt) {
   evt.currentTarget.classList.add('hide');
 });
 */
+
+
+
+
+
+//Lennin sekoilu:
+
+const url = 'http://127.0.0.1:5000/kilometria?id=1&km=';
+
+async function sade(evt) {
+  evt.preventDefault();
+
+  const maara = document.querySelector('#tasta').value;
+  console.log(maara);
+  let sadeUrl = url + maara;
+  console.log(sadeUrl);
+
+  const response = await fetch(sadeUrl);
+  const json = await response.json();
+  console.log(json);
+  const sade = json.km_lkm;
+  const vaihtoehdo = json.vaihtoehdot;
+  console.log(vaihtoehdo);
+  for (let i = 0; i <= 9; i++) {
+    const kord1 = json.vaihtoehdot[i]['latitude_deg'];
+    const kord2 = json.vaihtoehdot[i]['longitude_deg'];
+    console.log(kord1)
+    console.log(kord2)
+
+  }
+
+  const elementti = document.querySelector('#tahan');
+  elementti.innerText = sade;
+}
+
+const nappi2 = document.querySelector('#paina');
+nappi2.addEventListener('click', sade);
