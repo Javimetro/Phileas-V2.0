998e31780552caef9851e8b414685f31ee24cf45
javimetro <javierj@metropolia.fi>
2022-12-09 12:45:52 +0200

    class airport

--- backend/airport.py
+++ backend/airport.py
@@ -5,29 +5,54 @@ from geopy import distance
 
 
 class Airport:
-    # lisätty data, jottei tartte jokaista lentokenttää hakea erikseen
-    def __init__(self, ident, active=False, data=None):
-        self.ident = ident
-        self.active = active
 
-        # vältetään kauhiaa määrää hakuja
-        if data is None:
-            # find airport from DB
-            sql = "SELECT ident, name, latitude_deg, longitude_deg FROM Airport WHERE ident='" + ident + "'"
-            print(sql)
-            cur = config.conn.cursor()
-            cur.execute(sql)
-            res = cur.fetchall()
-            if len(res) == 1:
-                # game found
-                self.ident = res[0][0]
-                self.name = res[0][1]
-                self.latitude = float(res[0][2])
-                self.longitude = float(res[0][3])
-        else:
-            self.name = data['name']
-            self.latitude = float(data['latitude'])
-            self.longitude = float(data['longitude'])
+
+    def __init__(self,userId):
+        self.userId = userId
+
+
+    def alku(self, userId):
+        sql = f'''UPDATE game SET location= "EGLC" WHERE id={userId}'''
+        kursori = config.conn.cursor()
+        kursori.execute(sql)
+        tulos = kursori.fetchone()
+        print(sql)
+        print(tulos)
+
+    def haeLatLong(self, userId):
+        sql = f'''select latitude_deg, longitude_deg from airport, game where game.id={userId} and location = ident'''
+        kursori = config.conn.cursor()
+        kursori.execute(sql)
+        tulos = kursori.fetchall()
+        self.lat = float(tulos[0][0])
+        self.long = float(tulos[0][1])
+
+
+
+
+    # lisätty data, jottei tartte jokaista lentokenttää hakea erikseen
+    # def __init__(self, ident, active=False, data=None):
+    #     self.ident = ident
+    #     self.active = active
+    #
+    #     # vältetään kauhiaa määrää hakuja
+    #     if data is None:
+    #         # find airport from DB
+    #         sql = "SELECT ident, name, latitude_deg, longitude_deg FROM Airport WHERE ident='" + ident + "'"
+    #         print(sql)
+    #         cur = config.conn.cursor()
+    #         cur.execute(sql)
+    #         res = cur.fetchall()
+    #         if len(res) == 1:
+    #             # game found
+    #             self.ident = res[0][0]
+    #             self.name = res[0][1]
+    #             self.latitude = float(res[0][2])
+    #             self.longitude = float(res[0][3])
+    #     else:
+    #         self.name = data['name']
+    #         self.latitude = float(data['latitude'])
+    #         self.longitude = float(data['longitude'])
 
 
     def find_nearby_airports(self):
