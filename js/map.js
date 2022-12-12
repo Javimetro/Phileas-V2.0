cac46faa80e00be0b761290985d96c458ac054e0
elenablinova <leena.blinova@gmail.com>
2022-12-12 21:35:28 +0200

    changed map setView

--- js/map.js
+++ js/map.js
@@ -6,7 +6,7 @@ L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
   maxZoom: 20,
   subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
 }).addTo(map);
-map.setView([60, 24], 5);
+map.setView([53, 24], 5);
 
 const apiUrl = 'http://127.0.0.1:5000/';
 
